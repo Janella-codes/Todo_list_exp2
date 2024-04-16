0f4a332718987ea1c005be843156524892622125
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2024-04-16 11:25:20 -0700

    Add auth

--- prisma/migrations/20240413162656_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "todo" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20240413162929_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "todos" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
-);
